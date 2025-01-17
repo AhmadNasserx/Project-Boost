a1d84188e021339b09d50aace710b4fcf09aa048
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-01-17 13:55:21 +0200

    Controlling the player

--- player.gd
+++ player.gd
@@ -8,4 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+		
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
