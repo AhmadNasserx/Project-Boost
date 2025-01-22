716f954bbc2fbaa2ab4acfa941ba1b1143e44c3d
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-01-22 10:11:19 +0200

    editing script

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends RigidBody3D
 
-@export var force = 1000.0
-@export var torque_force = 100.0
+@export_range(750, 3000) var Thrust: float = 1000.0
+@export var torque_thrust: float = 200.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,8 +10,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * force)
+		apply_central_force(basis.y * delta * Thrust)
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0,0.0, torque_force * delta))
+		apply_torque(Vector3(0.0,0.0, torque_thrust * delta))
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0,0.0, -torque_force * delta))
+		apply_torque(Vector3(0.0,0.0, -torque_thrust * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Goal"):
+		pass
+	if body.is_in_group("Hazard"):
+		pass
