54a75cf194e4f77a29d3cfd4ea6d91a9119f593d
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-01-22 09:38:18 +0200

    updating player script

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,7 @@
-extends Node3D
+extends RigidBody3D
 
-
+@export var force = 1000.0
+@export var torque_force = 100.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,9 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
-		
+		apply_central_force(basis.y * delta * force)
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+		apply_torque(Vector3(0.0,0.0, torque_force * delta))
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+		apply_torque(Vector3(0.0,0.0, -torque_force * delta))
