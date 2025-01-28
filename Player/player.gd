f458cb160930e5d7d8890c2c728a17b897e8781c
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-01-28 10:23:21 +0200

    particles

--- Player/player.gd
+++ Player/player.gd
@@ -5,6 +5,11 @@ extends RigidBody3D
 
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $rightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $leftBoosterParticles2
+
 
 var is_transitioning: bool = false
 
@@ -17,10 +22,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * Thrust)
+		booster_particles.emitting = true
+		if !rocket_audio.playing:
+			rocket_audio.play()
+	else:
+			rocket_audio.stop()
+			booster_particles.emitting = false
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0,0.0, torque_thrust * delta))
+		right_booster_particles.emitting = true
+	else:
+		right_booster_particles.emitting = false
 	if Input.is_action_pressed("rotate_right"):
+		left_booster_particles.emitting = true
 		apply_torque(Vector3(0.0,0.0, -torque_thrust * delta))
+	else:
+		left_booster_particles.emitting = false
 
 
 func _on_body_entered(body: Node) -> void:
